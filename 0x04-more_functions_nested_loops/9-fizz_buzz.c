e04548a948f87818fccf303a45720fb75be91275
Robyn256 <robin.aceninja@gmail.com>
2023-02-23 15:46:14 -0800

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,24 +11,29 @@
 int main(void)
 {
 int i;
+char a[] = "Fizz";
+char b[] = "Buzz";
+char ab[] = "FizzBuzz";
 for (i = 1; i <= 100; i++)
 {
-if (i % 3 == 0 && i % 5 != 0)
+if (i == 100)
 {
-printf("Fizz");
-}
-else if (i % 5 == 0 && i % 3 != 0)
-{
-printf("Buzz");
+printf("%s", b);
 }
 else if (i % 3 == 0 i % 5 == 0)
 {
-printf("FizzBuzz");
+printf("%s", ab);
 }
-else if (i == 1)
+else if (i % 3 == 0)
 {
-printf("%d", i);
+printf("%s", a);
 }
+else if (i % 5 == 0)
+{
+printf("%s", b);
+}
+else
+printf("%s", i)
 }
 printf('\n');
 return (0);
